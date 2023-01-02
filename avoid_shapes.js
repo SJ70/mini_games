7ef3ee137f7a7b1d11ac3056c195d21a3b2c282d
SJ70 <seojae70@gmail.com>
2023-01-02 20:29:33 +0900

    .

--- avoid_shapes.js
+++ avoid_shapes.js
@@ -2,33 +2,31 @@ const canvas = document.getElementById('sandbox');
 const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+let em = canvas.width*canvas.height;
+let size = em / 30000;
+console.log(size);
 let rects = [];
 
 function Rect(){
-    this.size = Math.random()*10 + 20;
+    this.size = Math.floor(Math.random()*size/2) + size;
     this.x = this.size + Math.random() * (canvas.width - this.size*2);
     this.y = this.size + Math.random() * (canvas.height - this.size*2);
     this.dx = ((Math.random()>=0.5)?1:-1) * ( 1 +Math.random()*3);
     this.dy = ((Math.random()>=0.5)?1:-1) * ( 1 +Math.random()*3);
     this.angle = 0;
     this.spin = ((Math.random()>=0.5)?1:-1) * ( 0.1 + Math.random()*0.05 );
-    this.color = 'hsl('+Math.random()*360+',100%,75%)';
+    this.opacity = 0;
+    this.color = Math.random()*360;
+    
+    this.current_size = this.size * _spawningScale;
     this.spawning = 0;
+
     this.draw = function(){
         ctx.save();
-        ctx.strokeStyle = this.color;
+        ctx.strokeStyle = 'hsla('+this.color+',100%,80%,'+this.opacity+'%)';
         ctx.translate(this.x, this.y);
         ctx.rotate(this.angle);
-
-        if(this.spawning<1){
-            ctx.beginPath();
-            ctx.arc(0,0,1,0,Math.PI*2);
-            ctx.stroke();
-        }    
-        else{
-            ctx.strokeRect(0 - this.size/2, 0 - this.size/2, this.size, this.size);
-        }
-
+        ctx.strokeRect(0 - this.current_size/2, 0 - this.current_size/2, this.current_size, this.current_size);
         ctx.restore();
     }
     this.update = function(){
@@ -40,29 +38,41 @@ function Rect(){
         if(this.y >= canvas.height - this.size || this.y <= this.size)
             this.dy *= -1;
 
-        if(this.spawning<0){
-
-        }
-
         this.angle += this.spin;
-        console.log(this.x, this.y);        
+        // console.log(this.x, this.y);        
     }
+    this.reduceSize = function(){
+        if(this.current_size > this.size){
+            this.current_size -= this.size/2;
+            this.opacity += 2.5;
+            this.strokeStyle = 'hsla('+this.color+',100%,80%,'+this.opacity+'%)';
+            setTimeout(this.reduceSize,10);
+            console.log(111);
+        }
+    }
+}
+
+function drawScore(){
+    ctx.font = "1em";
+    ctx.fillStyle = 'white';
+    ctx.fillText = rects.length;
 }
 
 function Animate(){
     ctx.clearRect(0,0,canvas.width,canvas.height);
     for(let i=0; i<rects.length; i++){
+        drawScore();
         rects[i].update();
+        rects[i].reduceSize();
         rects[i].draw();
     }
     requestAnimationFrame(Animate);
 }
-
 Animate();
 
-let spawnrate = 1;
+
+const _spawningScale = 30;
 function addRect(){
-    if(rects.length>0) rects[rects.length-1].spawning = 1;
     rects.push(new Rect());
     setTimeout(addRect,1000);
 }
