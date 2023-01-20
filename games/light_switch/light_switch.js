26d5ffa1ebd2ea824c43d6b80fd9d5cb97a8ccb7
SJ70 <seojae70@gmail.com>
2023-01-20 17:20:35 +0900

    update : light_switch md

--- games/light_switch/light_switch.js
+++ games/light_switch/light_switch.js
@@ -1,16 +1,17 @@
 import Game from '../../essential/Game.js';
+import draw_switch from './draw_switch.js';
+import Line from './Line.js';
+import { _white_value, _black_value, _white, _black } from './colors.js';
 
-const _white_value = '240,240,240';
-const _black_value = '40,40,40';
-const _white = 'rgb('+_white_value+')';
-const _black = 'rgb('+_black_value+')';
+const canvas = document.getElementById('sandbox');
+const ctx = canvas.getContext('2d');
+const MAX_SPAWN_RATE = 100;
 
 export function light_switch(){
-    const canvas = document.getElementById('sandbox');
-    const ctx = canvas.getContext('2d');
     let game = new Game(canvas, ctx, 'light_switch', _black, '140,140,140', _white_value);
 
     let _light = true;
+    let lines = [];
 
     game.score.setFollowMouse(0.04, 0.03);
     game.score.setPluePos(0,-0.2);
@@ -38,23 +39,61 @@ export function light_switch(){
     // overriding
     game.gamestart = function(){
         game.gamestart_essential();
+        lines = [];
+        _spawn_rate = MAX_SPAWN_RATE;
     }
     game.gameover = function(){
         game.gameover_essential();
     }
     game.resize = function(){
-        game.score.setPluePos(0,-canvas.height*0.2);
         game.resize_essential();
     }
     game.resize();
+
+    let _spawn_rate = MAX_SPAWN_RATE;
+    let _time = _spawn_rate;
+    function do_spawn_line(){
+        console.log(_spawn_rate);
+        if(_time--<0){
+            _spawn_rate *= 0.999;
+            _time = _spawn_rate;
+            addLine();
+        }
+    }
+
+    function addLine(){
+        let speedDivisor = 200;
+        let dir_tmp = Math.random()*4;
+        let dir = (dir_tmp>3 ? 'up' : (dir_tmp>2 ? 'down' : (dir_tmp>1 ? 'left' : 'right')));
+        let time = 100;
+        let light = Math.random()>0.5;
+        lines.push(new Line(canvas, ctx, speedDivisor, dir, time, light));
+    }
+
+    function runLines(){
+        for(let i=0; i<lines.length; i++){
+            lines[i].move();
+            if(lines[i].isOnMiddle()){
+                if(lines[i].isVisible(_light)){
+                    game.gameover();
+                }
+                else{
+                    game.score.addScore();
+                }
+            }
+            if(lines[i].isVisible(_light)){
+                lines[i].draw();
+            }
+        }
+    }
 
     run();
     function run(){
         game.resetCanvas();
 
-
-
-        drawSwitch();
+        draw_switch(canvas,ctx,_light);
+        runLines();
+        if(game.isPlaying()) do_spawn_line();
         
         game.drawEssential(canvas.width/2, canvas.height/2);
 
@@ -63,72 +102,5 @@ export function light_switch(){
         requestAnimationFrame(run);
     }
 
-    function drawSwitch(){
-        drawSwitchBody();
-        drawMark();
-    }
-    function drawMark(){
-        //-
-        ctx.save();
-        ctx.translate(canvas.width/2, canvas.height/2);
-        let size = canvas.area*0.002;
-        ctx.fillStyle = _light ? 'hsl(0,0%,60%)' : 'hsl(0,0%,40%)';
-        ctx.beginPath();
-        ctx.fillRect(-size/2, ((_light) ? canvas.area*0.0185 : canvas.area*0.02 ) -size/2, size, size * 4 *(_light?0.75:1));
-        ctx.restore();
-
-        //o
-        ctx.save();
-        if(!_light) ctx.scale(1, 0.75);
-        ctx.translate(canvas.width/2, (_light) ? canvas.height/2 : canvas.height/2/0.75+canvas.area*0.004);
-
-        ctx.fillStyle = _light ? _black : _white;
-        ctx.beginPath();
-        ctx.arc(0,0,canvas.area*0.004,0,Math.PI*2);
-        ctx.fill();
-
-        ctx.fillStyle = _light ? 'hsl(0,0%,90%)' : 'hsl(0,0%,15%)';
-        ctx.beginPath();
-        ctx.arc(0,0,canvas.area*0.002,0,Math.PI*2);
-        ctx.fill();
-        ctx.restore();
-    }
-    function drawSwitchBody(){
-        ctx.save();
-        ctx.translate(canvas.width/2, canvas.height/2+canvas.area*0.012);
-        let size = canvas.area/10;
-        ctx.fillStyle = _light ? 'hsl(0,0%,90%)' : 'hsl(0,0%,15%)';
-        ctx.fillRect(-size/2, size/2, size, size/20);
-        ctx.fillStyle = _light ? 'hsl(0,0%,98%)' : 'hsl(0,0%,19%)';
-        ctx.fillRect(-size/2, -size/2, size, size);
-
-        let width = size*0.2;
-        let height = size*0.45;
-
-        if(_light){
-    
-            ctx.fillStyle = 'hsl(0,0%,100%)';
-            ctx.fillRect(-width/2, 0, width, height/2-height/8);
-            ctx.fillStyle = 'hsl(0,0%,90%)';
-            ctx.fillRect(-width/2, height/2-height/8, width, height/8);
-            ctx.fillStyle = 'hsl(0,0%,94%)';
-            ctx.fillRect(-width/2, height/2, width, height/8);
-            
-        }
-        else{
-    
-            ctx.fillStyle = 'hsl(0,0%,22%)';
-            ctx.fillRect(-width/2, -height/2, width, height/8);
-            ctx.fillStyle = 'hsl(0,0%,15%)';
-            ctx.fillRect(-width/2, -height/2+height/8, width, height/2-height/8);
-
-        }
-        ctx.strokeStyle = _light ? 'hsl(0,0%,85%)' : 'hsl(0,0%,10%)';
-        ctx.strokeRect(-size/2, -size/2, size, size);
-        ctx.strokeStyle = _light ? 'hsl(0,0%,80%)' : 'hsl(0,0%,5%)';
-        ctx.strokeRect(-width/2, -height/2, width, height);
-
-        ctx.restore();
-    }
 }
 export default light_switch;
